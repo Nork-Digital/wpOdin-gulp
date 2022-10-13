949828d067950cabfb17cf0da231052fdf8a3461
Caio Peres <caio@nork.digital>
2022-10-13 12:21:46 -0300

    Update optimize.php

--- inc/optimize.php
+++ inc/optimize.php
@@ -6,102 +6,107 @@
 /**
  * Cleanup wp_head().
  */
-function odin_head_cleanup() {
-	// Category feeds.
-	// remove_action( 'wp_head', 'feed_links_extra', 3 );
+function odin_head_cleanup()
+{
+    // Category feeds.
+    // remove_action( 'wp_head', 'feed_links_extra', 3 );
 
-	// Post and comment feeds.
-	// remove_action( 'wp_head', 'feed_links', 2 );
+    // Post and comment feeds.
+    // remove_action( 'wp_head', 'feed_links', 2 );
 
-	// EditURI link.
-	remove_action( 'wp_head', 'rsd_link' );
+    // EditURI link.
+    remove_action('wp_head', 'rsd_link');
 
-	// Windows live writer.
-	remove_action( 'wp_head', 'wlwmanifest_link' );
+    // Windows live writer.
+    remove_action('wp_head', 'wlwmanifest_link');
 
-	// Index link.
-	remove_action( 'wp_head', 'index_rel_link' );
+    // Index link.
+    remove_action('wp_head', 'index_rel_link');
 
-	// Previous link.
-	remove_action( 'wp_head', 'parent_post_rel_link', 10, 0 );
+    // Previous link.
+    remove_action('wp_head', 'parent_post_rel_link', 10, 0);
 
-	// Start link.
-	remove_action( 'wp_head', 'start_post_rel_link', 10, 0 );
+    // Start link.
+    remove_action('wp_head', 'start_post_rel_link', 10, 0);
 
-	// Links for adjacent posts.
-	remove_action( 'wp_head', 'adjacent_posts_rel_link_wp_head', 10, 0 );
+    // Links for adjacent posts.
+    remove_action('wp_head', 'adjacent_posts_rel_link_wp_head', 10, 0);
 
-	// WP version.
-	remove_action( 'wp_head', 'wp_generator' );
+    // WP version.
+    remove_action('wp_head', 'wp_generator');
 
-	// Emoji's
-	// remove_action( 'wp_head', 'print_emoji_detection_script', 7 );
-	// remove_action( 'admin_print_scripts', 'print_emoji_detection_script' );
-	// remove_action( 'wp_print_styles', 'print_emoji_styles' );
-	// remove_action( 'admin_print_styles', 'print_emoji_styles' );
-	// remove_filter( 'the_content_feed', 'wp_staticize_emoji' );
-	// remove_filter( 'comment_text_rss', 'wp_staticize_emoji' );
-	// remove_filter( 'wp_mail', 'wp_staticize_emoji_for_email' );
+    // Emoji's
+    // remove_action( 'wp_head', 'print_emoji_detection_script', 7 );
+    // remove_action( 'admin_print_scripts', 'print_emoji_detection_script' );
+    // remove_action( 'wp_print_styles', 'print_emoji_styles' );
+    // remove_action( 'admin_print_styles', 'print_emoji_styles' );
+    // remove_filter( 'the_content_feed', 'wp_staticize_emoji' );
+    // remove_filter( 'comment_text_rss', 'wp_staticize_emoji' );
+    // remove_filter( 'wp_mail', 'wp_staticize_emoji_for_email' );
 }
 
-add_action( 'init', 'odin_head_cleanup' );
+add_action('init', 'odin_head_cleanup');
 
 /**
  * Remove WP version from RSS.
  */
-add_filter( 'the_generator', '__return_false' );
+add_filter('the_generator', '__return_false');
 
 /**
  * Remove injected CSS for recent comments widget.
  */
-function odin_remove_wp_widget_recent_comments_style() {
-	if ( has_filter( 'wp_head', 'wp_widget_recent_comments_style' ) ) {
-		remove_filter( 'wp_head', 'wp_widget_recent_comments_style' );
-	}
+function odin_remove_wp_widget_recent_comments_style()
+{
+    if (has_filter('wp_head', 'wp_widget_recent_comments_style')) {
+        remove_filter('wp_head', 'wp_widget_recent_comments_style');
+    }
 }
 
-add_filter( 'wp_head', 'odin_remove_wp_widget_recent_comments_style', 1);
+add_filter('wp_head', 'odin_remove_wp_widget_recent_comments_style', 1);
 
 /**
  * Remove injected CSS from recent comments widget.
  */
-function odin_remove_recent_comments_style() {
-	global $wp_widget_factory;
+function odin_remove_recent_comments_style()
+{
+    global $wp_widget_factory;
 
-	if ( isset( $wp_widget_factory->widgets['WP_Widget_Recent_Comments'] ) ) {
-		remove_action( 'wp_head', array( $wp_widget_factory->widgets['WP_Widget_Recent_Comments'], 'recent_comments_style' ) );
-	}
+    if (isset($wp_widget_factory->widgets['WP_Widget_Recent_Comments'])) {
+        remove_action('wp_head', array( $wp_widget_factory->widgets['WP_Widget_Recent_Comments'], 'recent_comments_style' ));
+    }
 }
 
-add_action( 'wp_head', 'odin_remove_recent_comments_style', 1 );
+add_action('wp_head', 'odin_remove_recent_comments_style', 1);
 
 /**
  * Remove injected CSS from gallery.
  */
-add_filter( 'use_default_gallery_style', '__return_false' );
+add_filter('use_default_gallery_style', '__return_false');
 
 /**
  * Add rel="nofollow" and remove rel="category".
  */
-function odin_modify_category_rel( $text ) {
-	$search = array( 'rel="category"', 'rel="category tag"' );
-	$text = str_replace( $search, 'rel="nofollow"', $text );
+function odin_modify_category_rel($text)
+{
+    $search = array( 'rel="category"', 'rel="category tag"' );
+    $text = str_replace($search, 'rel="nofollow"', $text);
 
-	return $text;
+    return $text;
 }
 
-add_filter( 'wp_list_categories', 'odin_modify_category_rel' );
-add_filter( 'the_category', 'odin_modify_category_rel' );
+add_filter('wp_list_categories', 'odin_modify_category_rel');
+add_filter('the_category', 'odin_modify_category_rel');
 
 /**
  * Add rel="nofollow" and remove rel="tag".
  */
-function odin_modify_tag_rel( $taglink ) {
-	return str_replace( 'rel="tag">', 'rel="nofollow">', $taglink );
+function odin_modify_tag_rel($taglink)
+{
+    return str_replace('rel="tag">', 'rel="nofollow">', $taglink);
 }
 
-add_filter( 'wp_tag_cloud', 'odin_modify_tag_rel' );
-add_filter( 'the_tags', 'odin_modify_tag_rel' );
+add_filter('wp_tag_cloud', 'odin_modify_tag_rel');
+add_filter('the_tags', 'odin_modify_tag_rel');
 
 /**
  * Filter function used to remove the tinymce emoji plugin.
@@ -110,11 +115,12 @@ add_filter( 'the_tags', 'odin_modify_tag_rel' );
  *
  * @return array Difference betwen the two arrays
  */
-function disable_emojis_tinymce( $plugins ) {
-	return is_array( $plugins ) ? array_diff( $plugins, array( 'wpemoji' ) ) : array();
+function disable_emojis_tinymce($plugins)
+{
+    return is_array($plugins) ? array_diff($plugins, array( 'wpemoji' )) : array();
 }
 
-add_filter( 'tiny_mce_plugins', 'disable_emojis_tinymce' );
+add_filter('tiny_mce_plugins', 'disable_emojis_tinymce');
 
 
 /**
@@ -322,57 +328,57 @@ remove_action('welcome_panel', 'wp_welcome_panel');
 function login_styles()
 {
     ?>
-  <style type="text/css">
-    body {
-      background: #e8b800 !important;
-      /* Old browsers */
-      background: -moz-linear-gradient(45deg, #e8b800 0%, #fce48a 100%) !important;
-      /* FF3.6-15 */
-      background: -webkit-linear-gradient(45deg, #e8b800 0%, #fce48a 100%) !important;
-      /* Chrome10-25,Safari5.1-6 */
-      background: linear-gradient(45deg, #e8b800 0%, #fce48a 100%) !important;
-      /* W3C, IE10+, FF16+, Chrome26+, Opera12+, Safari7+ */
-      filter: progid: DXImageTransform.Microsoft.gradient(startColorstr='#e8b800', endColorstr='#000', GradientType=1);
-      /* IE6-9 fallback on horizontal gradient */
-      background-attachment: fixed !important;
-    }
+<style type="text/css">
+body {
+    background: #e8b800 !important;
+    /* Old browsers */
+    background: -moz-linear-gradient(45deg, #e8b800 0%, #fce48a 100%) !important;
+    /* FF3.6-15 */
+    background: -webkit-linear-gradient(45deg, #e8b800 0%, #fce48a 100%) !important;
+    /* Chrome10-25,Safari5.1-6 */
+    background: linear-gradient(45deg, #e8b800 0%, #fce48a 100%) !important;
+    /* W3C, IE10+, FF16+, Chrome26+, Opera12+, Safari7+ */
+    filter: progid: DXImageTransform.Microsoft.gradient(startColorstr='#e8b800', endColorstr='#000', GradientType=1);
+    /* IE6-9 fallback on horizontal gradient */
+    background-attachment: fixed !important;
+}
 
-    #wp-submit {
-      border: none !important;
-      box-shadow: none !important;
-      background: #000 !important;
-      text-shadow: none !important;
-      border-radius: 4px !important;
-      -webkit-border-radius: 4px !important;
-      color: #fff !important;
-      display: block;
-      width: 100% !important;
-      margin: 30px 0 0 0 !important;
-      font-size: 16px;
-      padding: 5px 0 !important;
-      height: auto !important;
-      transition: all 0.5s;
-    }
+#wp-submit {
+    border: none !important;
+    box-shadow: none !important;
+    background: #000 !important;
+    text-shadow: none !important;
+    border-radius: 4px !important;
+    -webkit-border-radius: 4px !important;
+    color: #fff !important;
+    display: block;
+    width: 100% !important;
+    margin: 30px 0 0 0 !important;
+    font-size: 16px;
+    padding: 5px 0 !important;
+    height: auto !important;
+    transition: all 0.5s;
+}
 
-    #wp-submit:hover {
-      background: #e8b800 !important;
-    }
+#wp-submit:hover {
+    background: #e8b800 !important;
+}
 
-    .login h1 a {
-      background-image: url('http://bbcreative.org/upload/imagens/889e3e8621c81383c4e5684e96a77049.png') !important;
-      background-image: url('http://bbcreative.org/upload/imagens/889e3e8621c81383c4e5684e96a77049.png') !important;
-      background-size: 100% !important;
-      background-position: center center !important;
-      background-repeat: no-repeat;
-      height: 74px !important;
-      width: 250px !important;
-    }
+.login h1 a {
+    background-image: url('http://bbcreative.org/upload/imagens/889e3e8621c81383c4e5684e96a77049.png') !important;
+    background-image: url('http://bbcreative.org/upload/imagens/889e3e8621c81383c4e5684e96a77049.png') !important;
+    background-size: 100% !important;
+    background-position: center center !important;
+    background-repeat: no-repeat;
+    height: 74px !important;
+    width: 250px !important;
+}
 
-    .login #backtoblog a,
-    .login #nav a {
-      color: #fff !important;
-    }
-  </style> 
+.login #backtoblog a,
+.login #nav a {
+    color: #fff !important;
+}
+</style>
 
 <?php }
 add_action('login_enqueue_scripts', 'login_styles');
@@ -505,3 +511,35 @@ function line_break_shortcode()
     return '<br />';
 }
 add_shortcode('br', 'line_break_shortcode');
+
+
+//Limita o upload
+/**
+ * Filter the upload size limit for non-administrators.
+ *
+ * @param string $size Upload size limit (in bytes).
+ * @return int (maybe) Filtered size limit.
+ */
+function filter_site_upload_size_limit($size)
+{
+    if (! current_user_can('manage_options')) {
+        // 10 MB.
+        $size = 1024 * 10000;
+    }
+    return $size;
+}
+add_filter('upload_size_limit', 'filter_site_upload_size_limit', 20);
+
+
+//desabilita extensões não otimizadas para web
+add_filter('upload_mimes', 'restrict_mime');
+function restrict_mime($mimes)
+{
+    $mimes = array(
+                'mov' => 'video/mov',
+                'avi' => 'video/avi',
+                'bmp' => 'image/bmp',
+                /* 'gif' => 'image/gif', */
+    );
+    return $mimes;
+}
